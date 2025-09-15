e98087841dcb7fedca6884d60e438aef76595dd7
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-09-15 14:50:17 +0300

    reset

--- emulator.py
+++ emulator.py
@@ -1,17 +1,8 @@
-import argparse
-import sys
 import clr
-import os
 
 clr.AddReference("System.Windows.Forms")
 import shlex
 from System.Windows.Forms import Form, TextBox, Keys, MessageBox
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--vfs-path", type=str)
-parser.add_argument("--script", type=str)
-args = parser.parse_args()
-print(args.script)
 
 form = Form()
 form.Text = "VFS"
@@ -25,42 +16,20 @@ def input_enter(sender, event):
         input.Text = ""
 
         mes = shlex.split(user_input)
-        if not mes: return
 
-        if mes[0].lower() == "ls":
+        if mes and mes[0].lower() == "ls":
             MessageBox.Show(f"Команда: ls\nАргументы: {mes[1:]}")
+            return
 
-        elif mes[0].lower() == "cd":
+        if mes and mes[0].lower() == "cd":
             MessageBox.Show(f"Команда: cd\nАргументы: {mes[1:]}")
+            return
 
-        elif mes[0].lower() == "exit":
+        if mes and mes[0].lower() == "exit":
             form.Close()
+            return
 
-        else:
-            MessageBox.Show(str(mes))
+        MessageBox.Show(str(mes))
 
 input.KeyDown += input_enter
-
-def file_enter(cmd_line):
-    mes = shlex.split(cmd_line)
-    if not mes: return
-
-    if mes[0].lower() == "ls":
-        MessageBox.Show(f"Команда: ls\nАргументы: {mes[1:]}")
-    elif mes[0].lower() == "cd":
-        MessageBox.Show(f"Команда: cd\nАргументы: {mes[1:]}")
-    elif mes[0].lower() == "exit":
-        MessageBox.Show("Завершение программы")
-        #form.Close()
-
-if args.script:
-    f = open(args.script)
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('#'):
-            continue
-        MessageBox.Show(f"vfs> {line}")
-        file_enter(line)
-    f.close()
-
 form.ShowDialog()
--- test2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call .\venv\Scripts\activate.bat
-python emulator.py --vfs-path "C:\myvfs" --script "start.vfs"
-pause
